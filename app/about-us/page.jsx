42cb149a59768322e6b01be3bdda3abb2e107857
mouus <musannif.git@gmail.com>
2025-02-06 15:05:27 +0500

    bug fix

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -56,10 +56,10 @@ function Page() {
                 {/* Mobile Menu */}
                 {menuOpen && (
                     <div className="md:hidden bg-slate-900 text-white flex flex-col gap-4 p-4">
-                        <Link href="/south-dives" className="hover:underline transition text-lg">
+                        <Link href="/dive-itineraries" className="hover:underline transition text-lg">
                             Dive Itineraries
                         </Link>
-                        <Link href="/south-dives" className="hover:underline transition text-lg">
+                        <Link href="/our-cruise" className="hover:underline transition text-lg">
                             Our Cruise
                         </Link>
                         <Link href="/about-us" className="hover:underline transition text-lg">
