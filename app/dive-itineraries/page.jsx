e7a51ed069495e26c4ef4513d5d056843f01d914
mouus <musannif.git@gmail.com>
2025-02-07 19:05:15 +0500

    free diving text change

--- app/dive-itineraries/page.jsx
+++ app/dive-itineraries/page.jsx
@@ -116,9 +116,9 @@ function DiveItinerariesPage() {
                 className="w-full h-64 object-cover"
               />
               <div className="p-6">
-                <h3 className="text-2xl font-bold mb-2">Coral Reef Adventure</h3>
+                <h3 className="text-2xl font-bold mb-2">Free Diving Fuvamulah</h3>
                 <p className="text-gray-700 mb-4">
-                  Experience vibrant coral reefs and diverse marine life on this
+                  Experience vibrant free diving and diverse marine life on this
                   thrilling dive itinerary.
                 </p>
                 <Link href={'/book'} className="bg-blue-800 text-white py-2 px-4 rounded hover:bg-blue-900 transition">
